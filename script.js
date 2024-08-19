b1d7a77a088b34d750b3d01eaca594ee7f662523
gabiclaudino <106687149+gabiclaudino@users.noreply.github.com>
2024-08-19 11:34:18 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,67 +6,67 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Qual foi o ano em que a World Wide Web foi lançada para o público?",
         alternativas: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                texto: "1989",
+                afirmacao: "1991"
             },
             {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
+                texto: "1995",
+                afirmacao: "2000"
             }
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Qual é o protocolo principal utilizado para a transmissão de páginas web?",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                texto: "FTP",
+                afirmacao: "SMTP"
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                texto: "HTTP",
+                afirmacao: "TCP/IP"
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "Quais são os diferentes tipos de sistemas de aquecimento residencial e quais são suas principais vantagens e desvantagens?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "Aquecimento a gás, aquecimento elétrico e aquecimento a lenha.",
+                afirmacao: "Aquecimento solar, aquecimento geotérmico e aquecimento  a óleo."
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: "Aquecimento a água quente, aquecimento por ar forçado e aquecimento  radiadores.",
+                afirmacao: "Aquecimento a carvão, aquecimento por infravermelho e aquecimento por energia eólica."
             }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "Como o isolamento térmico de uma casa pode influenciar a eficiência ao reduzir a perda de calor e a  necessidade de usar o sistema de aquecimento?",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                texto: "Melhora a eficiência ao reduzir  a perda de calor e a necessidade de ussar o sistema de aquecimento com frequência.",
+                afirmacao: "Reduz a eficiência ao fazer com que o sistema de aquecimento trabalhe mais para manter a temperatura."
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                texto: "Não tem impacto significativo na eficiência do sistema de aquecimento.",
+                afirmacao: "Aumenta a umidade dentro da casa, tornando o sistema de aquecimento menos eficiente."
             }
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "Qual é a diferença entre aquecimento central e aquecimento por radiadores individuais, e qual sistema é mais adequado para diferentes tipos de construções? ",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                texto: "O aquecimento central usa um único sistema para aquecer toda a casa, enquanto os radiadores individuais aquecem apenas cômodos específicos.",
+                afirmacao: "O aquecimento central é menos eficiente do que os radiadores individuais porque aquece somente uma área da casa."
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                texto: "Radiadires individuais usam energia solar, enquanto o aquecimento central utiliza eletricidade.",
+                afirmacao: "O aquecimento central é ideal para construções pequemas e os radiadores individuais para grandes indíviduos."
             }
         ]
     },
